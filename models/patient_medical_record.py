83091d4fbd1728bed74c3b4c05424692b999e309
Daniel Blanco <daniel@blancomartin.cl>
2017-04-14 10:50:36 -0300

    fix tab

--- models/patient_medical_record.py
+++ models/patient_medical_record.py
@@ -9,14 +9,14 @@ class medical_record_patient(models.Model):
     _inherit = 'medical.patient'
 
     medical_record = fields.Text(string='Medical Record')
-	medical_record_history = fields.Text(
-		string='Medical Record', compute='_add_medical_record', store=True,
-		readonly=True)
+    medical_record_history = fields.Text(
+        string='Medical Record', compute='_add_medical_record', store=True,
+        readonly=True)
 
-	@api.multi
-	@api.depends('medical_record')
-	def _add_medical_record(self):
-		for x in self:
-			x.medical_record_history += "\n{} - {}\n{}".format(
-				datetime.now(), x.company_id.user_id.name, x.medical_record)
-			x.medical_record = False
+    @api.multi
+    @api.depends('medical_record')
+    def _add_medical_record(self):
+        for x in self:
+            x.medical_record_history += "\n{} - {}\n{}".format(
+                datetime.now(), x.company_id.user_id.name, x.medical_record)
+            x.medical_record = False
